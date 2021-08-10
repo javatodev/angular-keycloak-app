d4b0ad5d2cd24dee20500737207af382cd33f7f9
javatodev <sendtospringboot@gmail.com>
2021-08-10 23:33:33 +0530

    base UI components are done

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  loggedUserName = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  logout(): void {
+  }
+
+}
